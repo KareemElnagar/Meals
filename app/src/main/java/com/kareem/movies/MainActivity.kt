1fd49712e6dcadf7c830034240b189ae63e6c49f
Kareem Elnagar <kareemelnagar733@gmail.com>
2024-06-11 06:36:59 +0200

    fix

--- app/src/main/java/com/kareem/movies/MainActivity.kt
+++ app/src/main/java/com/kareem/movies/MainActivity.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.kareem.movies.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MealsViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
+        val viewModel: MealsViewModel by viewModels()
+        val rv = findViewById<RecyclerView>(R.id.rv)
         val mealsAdapter = MealsAdapter()
 
         viewModel.getMeals()
         lifecycleScope.launch {
             viewModel.categories.collect {
                 mealsAdapter.submitList(it?.categories)
-                binding.rv.adapter = mealsAdapter
+                rv.adapter = mealsAdapter
 
             }
         }
